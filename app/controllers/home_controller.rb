9d18ca2e0c080b3232a93967f3072e633895f99b
Igor Okulist <okigan@yahoo.com>
2011-04-17 00:39:20 -0700

    Add outofdate method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
     def index
         @requirements = Requirement.all
+        @toplevel_requirements = Requirement.rootlevel
         @traces = Trace.all
     end
 end
--- app/models/requirement.rb
+++ app/models/requirement.rb
@@ -1,4 +1,16 @@
 class Requirement < ActiveRecord::Base
     belongs_to :parent, :class_name => "Requirement", :foreign_key => "parent_requirement_id"
     has_many :children, :class_name => "Requirement", :foreign_key => "parent_requirement_id"
+    
+    scope :rootlevel    , where(:parent_requirement_id => nil)
+    
+    scope :lowerlevel, where("parent_requirement_id <> ?", 0)
+    
+    def outofdate
+        if self.parent == nil
+            return false;
+        else
+            return self.parent.updated_at > updated_at
+        end
+    end
 end
